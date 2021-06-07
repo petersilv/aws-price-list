ca83ae45b740da04d70b142c818d400467c75516
psilv <p.silvester.94@gmail.com>
2021-06-07 18:10:30 +0100

    Update required Terraform version in Snowflake Integration module

--- terraform/modules/sno_integration/_main.tf
+++ terraform/modules/sno_integration/_main.tf
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 terraform {
 
-  required_version = ">= 0.15.1"
+  required_version = ">= 0.15.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
