20714f8fcbe582e3661ac339ae6aedb83b81f3d7
psilv <p.silvester.94@gmail.com>
2021-08-17 10:22:36 +0100

    Add Product Pricing and Terms models

---
+++ dbt/models/products/product_pricing.sql
@@ -0,0 +1,21 @@
+{#- ----------------------------------------------------------------------------
+Run query -#}
+
+select a.service_code
+     , a.product_family
+     , a.product_sku
+     , c.value:offerTermCode :: string as offer_term_code
+     , d.value:rateCode      :: string as rate_code
+     , d.value:appliesTo     :: string as applies_to
+     , d.value:unit          :: string as unit
+     , d.value:description   :: string as description
+     , d.value:endRange      :: string as end_range
+     , d.value:beginRange    :: string as begin_range
+     , e.value               :: float  as price_per_unit
+     , e.key                 :: string as price_per_unit_currency
+
+  from {{ref('product_skus')}} a
+     , lateral flatten (input => a.terms) b
+     , lateral flatten (input => b.value) c
+     , lateral flatten (input => c.value:priceDimensions) d
+     , lateral flatten (input => d.value:pricePerUnit) e
---
+++ dbt/models/products/product_terms.sql
@@ -0,0 +1,16 @@
+{#- ----------------------------------------------------------------------------
+Run query -#}
+
+select a.service_code
+     , a.product_family
+     , a.product_sku
+     , c.value:offerTermCode                      ::string    as offer_term_code
+     , b.key                                      ::string    as pricing_type
+     , c.value:effectiveDate                      ::timestamp as effective_date
+     , c.value:termAttributes:LeaseContractLength ::string    as lease_contract_length
+     , c.value:termAttributes:OfferingClass       ::string    as offering_class
+     , c.value:termAttributes:PurchaseOption      ::string    as purchase_option
+
+  from {{ref('product_skus')}} a
+     , lateral flatten (input => a.terms) b
+     , lateral flatten (input => b.value) c
