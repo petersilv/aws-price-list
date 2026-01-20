df29628f3f5c23b8b8dfb76b1edf5a587c6275a0
Peter Silvester <p.silvester.94@gmail.com>
2026-01-20 09:31:31 +0000

    Remove old files / folders

--- dbt/models/products/product_attributes_narrow.sql
+++
@@ -1,8 +0,0 @@
-select a.service_code
-     , a.product_family
-     , a.product_sku
-     , b.key as key_name
-     , b.value ::string as value
-
-  from {{ref('product_skus')}} a
-     , lateral flatten (input => a.attributes) b
